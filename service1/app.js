aa54269864e3b863ee26dfb4d7b39e3e27d9f1fd
SeungWookHan <hswook12@me.com>
2022-01-19 18:59:26 +0900

    Connect service1 with message-queue

--- service1/app.js
+++ service1/app.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const rabbit = require('./amqpClient')
+
 app.get('/', (req, res) => {
   res.send('Hello World! This is server1')
 })
+
+// app.post('/send', async (req, res)=> {
+//   const broker = await rabbit.getInstance()
+//   await broker.send('test', Buffer.from(JSON.stringify(ctx.request.body)))
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://0.0.0.0:${port}`)
