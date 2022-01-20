2b3d9efe7dc8b5d4c31ed6b6610c29eee698d9f4
SeungWookHan <hswook12@me.com>
2022-01-20 15:03:22 +0900

    Add sendToQueue to send messages to the queue

--- service1/amqpClient.js
+++ service1/amqpClient.js
@@ -3,11 +3,13 @@ const QUEUE_NAME = 'square';
 const EXCHANGE_TYPE = 'direct';
 const EXCHANGE_NAME = 'main';
 const KEY = 'myKey';
-// const number = '5'
+const number = '5'
+
 connection = rabbit.connect('amqp://rabbitmq');
 connection.then(async (conn)=>{
    const channel = await conn.createChannel();
    await channel.assertExchange(EXCHANGE_NAME, EXCHANGE_TYPE);
    await channel.assertQueue(QUEUE_NAME);
    channel.bindQueue(QUEUE_NAME, EXCHANGE_NAME, KEY);
+   channel.sendToQueue(QUEUE_NAME, Buffer.from(number))
 })
