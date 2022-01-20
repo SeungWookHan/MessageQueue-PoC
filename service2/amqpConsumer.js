cc28b27da7d509635dee4f9413a31af07dedf712
SeungWookHan <hswook12@me.com>
2022-01-20 15:29:01 +0900

    Add consumer to consume the content

---
+++ service2/amqpConsumer.js
@@ -0,0 +1,13 @@
+const rabbit = require('amqplib');
+const QUEUE_NAME = 'square';
+
+connection = rabbit.connect('amqp://rabbitmq');
+connection.then(async (conn)=>{
+   const channel = await conn.createChannel();
+   channel.consume(QUEUE_NAME, (m)=>{
+       const number = parseInt(m.content.toString())
+       const square = number * number
+       console.log(square)
+       channel.ack(m)
+   })
+})
--- service2/app.js
+++ service2/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3001
 
+const rabbit = require('./amqpConsumer')
+
 app.get('/', (req, res) => {
   res.send('Hello World! This is server2')
 })
